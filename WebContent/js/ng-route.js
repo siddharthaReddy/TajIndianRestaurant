ed7c68a757973314061726c882ac35387fdc3afd
siddharthaReddy <siddhartha.reddy@outlook.com>
2015-02-09 21:16:32 -0700

    Menu Page accordin fixing.

--- WebContent/js/ng-route.js
+++ WebContent/js/ng-route.js
@@ -1,42 +1,48 @@
 
-var myapp = angular.module('myapp',['ngRoute']);
+var myapp = angular.module('myapp',['ngRoute','ui.bootstrap']);
 
 myapp.config(function($routeProvider){
 	$routeProvider
 	.when('/',{
-		templateUrl: 'home.html',
+		templateUrl: 'pages/home.html',
 		controller: 'HomeController'
 	})
 	.when('/menu',{
-		templateUrl: 'menu.html',
+		templateUrl: 'menuitems.html',
 		controller: 'MenuController'
 	})
 	.when('/gallery',{
-		templateUrl: 'gallery.html',
+		templateUrl: 'pages/gallery.html',
 		controller: 'GalleryController'
 	})
 	.when('/contact',{
-		templateUrl: 'contact.html',
+		templateUrl: 'pages/contact.html',
 		controller: 'ContactController'
 	})
 	.when('/about',{
-		templateUrl: 'about.html',
+		templateUrl: 'pages/about.html',
 		controller: 'AboutController'
 	});
 });
 
-myapp.controller('HomeController', ['$scope','$log', function($scope, $log){
+myapp.controller('HomeController', ['$scope',function($scope){
 	$scope.name= "Home";
 }]);
-myapp.controller('MenuController', ['$scope','$log', function($scope, $log){
-	$scope.name= "Menu";	
+
+myapp.controller('MenuController', ['$scope',"$http", function($scope,$http){
+	$http.get("menu.json")
+    .success(function(response) {$scope.data = response;});
 }]);
-myapp.controller('GalleryController', ['$scope','$log', function($scope, $log){
+	
+myapp.controller('GalleryController', ['$scope',function($scope){
 	
 }]);
-myapp.controller('ContactController', ['$scope','$log', function($scope, $log){
+
+myapp.controller('ContactController', ['$scope',function($scope){
 	
 }]);
-myapp.controller('AboutController', ['$scope','$log', function($scope, $log){
+
+myapp.controller('AboutController', ['$scope',function($scope){
 	
 }]);
+
